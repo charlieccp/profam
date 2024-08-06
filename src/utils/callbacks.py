2c95c06c6a70ada2699374a8eb18ad2589be60b0
alex-hh <alex.hhooker@gmail.com>
2024-08-06 21:26:19 +0100

    add print callback

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -5,3 +5,18 @@ class ShuffleCallback(Callback):
     def on_train_epoch_start(self, trainer, pl_module):
         # https://huggingface.co/docs/datasets/v2.20.0/en/package_reference/main_classes#datasets.Dataset.to_iterable_dataset
         trainer.train_dataloader.dataset.set_epoch(trainer.current_epoch)
+
+
+class PrintCallback(Callback):
+    def __init__(self, print_freq=1):
+        self.print_freq = print_freq
+
+    def on_train_epoch_end(self, trainer, pl_module):
+        if self.print_freq > 0 and (
+            (pl_module.current_epoch + 1) % self.print_freq == 0
+        ):
+            metrics = trainer.callback_metrics
+            metrics_msg = "\t".join([f"{k}: {v:.3f}" for k, v in metrics.items()])
+            print(
+                f"Epoch {pl_module.current_epoch}, metrics:\t{metrics_msg}", flush=True
+            )
