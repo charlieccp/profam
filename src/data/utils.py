2367aab34e501950a2dddf3f000d39d52c47151c
alex-hh <alex.hhooker@gmail.com>
2024-08-14 16:50:50 +0100

    refactor load dataset for better maintainability

--- src/data/utils.py
+++ src/data/utils.py
@@ -1,37 +1,13 @@
 import bisect
 import glob
-import hashlib
 import itertools
 import os
 import random
-from dataclasses import dataclass
-from typing import Any, Dict, List, Optional
+from typing import Any, List, Optional
 
 import numpy as np
 import torch
-from datasets import Dataset, load_dataset
-from omegaconf.listconfig import ListConfig
 from transformers import DataCollatorForLanguageModeling, PreTrainedTokenizerFast
-
-from src.data.fasta import convert_sequence_with_positions, read_fasta_sequences
-
-
-# TODO: in future we might actually want standalone dataset class for
-# more flexible customisation (e.g. mapping uniprot ids via db)
-@dataclass
-class ProteinDatasetConfig:
-    name: str
-    keep_gaps: bool = False
-    data_path_pattern: Optional[str] = None
-    holdout_data_files: Optional[str] = None
-    data_path_file: Optional[str] = None
-    keep_insertions: bool = False
-    to_upper: bool = False
-    file_repeats: int = 1
-    is_parquet: bool = False
-    minimum_sequences: Optional[int] = None
-    document_tag: str = "[RAW]"
-    truncate_after_n_sequences: Optional[int] = None
 
 
 class StringObject:
@@ -124,228 +100,9 @@ def get_seq_pos_from_positions(
     return seq_pos
 
 
-def subsample_fasta_lines(lines, n_lines, shuffle=True):
-    start_ix = np.array([i for i, l in enumerate(lines) if l[0] == ">"])
-    end_ix = start_ix[1:]
-    end_ix = np.append(end_ix, len(lines))
-    lines_per_seq = len(lines) // len(start_ix)
-    n_samples = min(n_lines // lines_per_seq, len(start_ix))
-    if shuffle:
-        sample_indices = np.random.choice(len(start_ix), n_samples, replace=False)
-    else:
-        sample_indices = np.arange(n_samples)
-    starts = start_ix[sample_indices]
-    ends = end_ix[sample_indices]
-    sampled_lines = []
-    for start, end in zip(starts, ends):
-        assert lines[end - 1][0] != ">"
-        sampled_lines.extend(lines[start:end])
-    return sampled_lines
-
-
-def load_protein_dataset(
-    cfg: ProteinDatasetConfig,
-    tokenizer: PreTrainedTokenizerFast,
-    max_tokens: Optional[int] = 5000,
-    data_dir="../data",
-    split="train",
-    include_doc_hashes: bool = False,
-    use_seq_pos: bool = False,
-    max_seq_pos: int = 1024,
-    shuffle: bool = True,
-) -> Dataset:
-    def preprocess_fasta(example: Dict[str, Any]) -> Dict[str, Any]:
-        # N.B. for stockholm format we need to check that sequences aren't split over
-        # multiple lines
-        if "sequences" in example:
-            sequence_iterator = example["sequences"]
-            max_sequences_to_preprocess = max_tokens // 10
-            if len(sequence_iterator) > max_sequences_to_preprocess:
-                selected_indices = np.random.choice(
-                    len(sequence_iterator), max_sequences_to_preprocess, replace=False
-                )
-                sequence_iterator = [sequence_iterator[i] for i in selected_indices]
-        else:
-            lines = example["text"].split("\n")
-            if not len(lines[-1]):
-                lines = lines[:-1]
-            # min 2 lines per seq, assume at least 10 tks per line
-            max_fasta_lines_to_preprocess = (
-                max_tokens // 5
-            )  # upper bound on lines to proc.
-            if len(lines) > max_fasta_lines_to_preprocess:
-                lines = subsample_fasta_lines(
-                    lines,
-                    max_fasta_lines_to_preprocess,
-                    shuffle=shuffle,
-                )
-            sequence_iterator = read_fasta_sequences(
-                lines,
-                # preserve original sequences before getting positions
-                keep_gaps=True if use_seq_pos else cfg.keep_gaps,
-                keep_insertions=True if use_seq_pos else cfg.keep_insertions,
-                to_upper=False if use_seq_pos else cfg.to_upper,
-            )
-        if use_seq_pos:
-            sequences = []
-            positions = []
-            for seq in itertools.islice(
-                sequence_iterator, cfg.truncate_after_n_sequences
-            ):
-                seq, pos, _ = convert_sequence_with_positions(
-                    seq,
-                    keep_gaps=cfg.keep_gaps,
-                    keep_insertions=cfg.keep_insertions,
-                    to_upper=cfg.to_upper,
-                )
-                sequences.append(seq)
-                positions.append(pos)
-            sequences, positions = sample_to_max_tokens(
-                sequences, positions=positions, max_tokens=max_tokens, shuffle=shuffle
-            )
-
-        else:
-            sequences = [
-                seq
-                for seq in itertools.islice(
-                    sequence_iterator, cfg.truncate_after_n_sequences
-                )
-            ]  # necessary for fasta iterator...
-            sequences = sample_to_max_tokens(
-                sequences, max_tokens=max_tokens, shuffle=shuffle
-            )
-
-        concatenated_seqs = (
-            cfg.document_tag
-            + tokenizer.bos_token
-            + tokenizer.sep_token.join(sequences)
-            + tokenizer.sep_token
-        )
-        tokenized = tokenizer(
-            concatenated_seqs,
-            truncation=False,  # shouldnt be necessary: bisection should handle
-            max_length=max_tokens,
-            return_tensors="pt",
-            # padding="longest",
-            padding="max_length",
-            add_special_tokens=False,
-        )
-        if max_tokens is not None:
-            assert tokenized.input_ids.shape[1] <= max_tokens, (
-                tokenized.input_ids.shape[1],
-                max_tokens,
-            )
-
-        tokenized.data = {k: v.squeeze() for k, v in tokenized.data.items()}
-        # tokenized.input_ids is flat now
-        tokenized.data["ds_name"] = cfg.name
-        tokenized.data["total_num_sequences"] = len(sequences)  # below length threshold
-        if include_doc_hashes:
-            # identify documents by a hash of the first 512 characters
-            tokenized.data["doc_hash"] = hashlib.md5(
-                example["text"][:512].encode()
-            ).hexdigest()
-
-        if use_seq_pos:
-            seq_pos = get_seq_pos_from_positions(
-                tokenized.input_ids,
-                positions,
-                pad_token_id=tokenizer.pad_token_id,
-                max_seq_pos=max_seq_pos,
-                num_start_tokens=2,
-            )
-            tokenized.data["seq_pos"] = seq_pos
-
-        return tokenized
-
-    def batched_preprocess_and_filter(batch):
-        batch_dict = {}
-        for example_text in batch["text"]:
-            example = {"text": example_text}
-            processed = preprocess_fasta(example).data
-            if (
-                cfg.minimum_sequences is None
-                or processed["total_num_sequences"] >= cfg.minimum_sequences
-            ):
-                for k, v in processed.items():
-                    if k not in batch_dict:
-                        batch_dict[k] = []
-                    batch_dict[k].append(v)
-        print(
-            len(batch["text"]),
-            len(batch_dict["ds_name"]) if "ds_name" in batch_dict else 0,
-        )
-        return batch_dict
-
-    if cfg.data_path_pattern is not None:
-        # replace hf path resolution with manual glob, to allow repetition
-        # https://github.com/huggingface/datasets/blob/98fdc9e78e6d057ca66e58a37f49d6618aab8130/src/datasets/data_files.py#L323
-        data_files = glob.glob(os.path.join(data_dir, cfg.data_path_pattern))
-    else:
-        assert cfg.data_path_file is not None
-        with open(os.path.join(data_dir, cfg.data_path_file), "r") as f:
-            data_files = [
-                os.path.join(data_dir, data_file) for data_file in f.read().splitlines()
-            ]
-
-    if cfg.holdout_data_files is not None:
-        assert isinstance(cfg.holdout_data_files, list) or isinstance(
-            cfg.holdout_data_files, ListConfig
-        ), f"holdout files is {type(cfg.holdout_data_files)} not list"
-        all_files = len(data_files)
-        data_files = [f for f in data_files if f not in cfg.holdout_data_files]
-        print("Excluding", all_files - len(data_files), "holdout files")
-
-    assert isinstance(data_files, list)
-    data_files = data_files * cfg.file_repeats
-    random.shuffle(data_files)  # TODO: seed explicitly?
-    print(
-        f"Loading {cfg.name} dataset from {len(data_files)} files, "
-        f"({cfg.file_repeats} repeats), "
-        f"{os.path.join(data_dir, cfg.data_path_pattern)}"
-    )
-    if cfg.is_parquet:
-        dataset = load_dataset(
-            path="parquet",
-            data_files=data_files,
-            split=split,
-            streaming=True,
-            verification_mode="no_checks",
-        )
-    else:
-        # THIS STEP IS SLOW FOR GYM MSAS (V LARGE FILES) --- BUT WHY - WHAT HAPPENS?
-        dataset = load_dataset(
-            "text",
-            data_files=data_files,
-            split=split,
-            streaming=True,
-            sample_by="document",
-        )
-    print("Dataset n shards", dataset.n_shards)
-    print("Verifying dataset content:")
-    for i, item in enumerate(dataset.take(3)):
-        print(f"  Item {i + 1}:")
-        for key, value in item.items():
-            print(f"    {key}: {value[:100] if isinstance(value, str) else value}")
-        print()
-    # with batched map there is a massive delay before training actually starts - why?
-    # dataset = dataset.map(
-    #     batched_preprocess_and_filter,
-    #     batched=True,
-    #     remove_columns=["text"],
-    #     batch_size=2,
-    # )
-    # filter after map also seems to slow things down...
-    dataset = dataset.map(
-        preprocess_fasta, batched=False, remove_columns=["text"]
-    ).filter(lambda x: x["total_num_sequences"] >= (cfg.minimum_sequences or 1))
-
-    return dataset
-
-
 def sample_to_max_tokens(
     sequences,
-    positions: Optional[List[int]] = None,
+    extra_arrays: Optional[List[List[Any] | np.ndarray]] = None,
     max_tokens: Optional[int] = None,
     shuffle=True,
     seed: Optional[int] = None,
@@ -355,12 +112,14 @@ def sample_to_max_tokens(
     # TODO: implement keep first, drop first
     if drop_first:
         sequences = sequences[1:]
-        if positions is not None:
-            positions = positions[1:]
+        if extra_arrays is not None:
+            extra_arrays = [arr[1:] for arr in extra_arrays]
 
     if shuffle:
         perm = rng.permutation(len(sequences))
         sequences = [sequences[i] for i in perm]
+        if extra_arrays is not None:
+            extra_arrays = [[arr[i] for i in perm] for arr in extra_arrays]
 
     if max_tokens is not None:
         cumulative_lengths = list(
@@ -372,7 +131,9 @@ def sample_to_max_tokens(
         )  # -2 for doc start and end tokens
     else:
         insertion_point = len(sequences)
-    if positions is None:
+    if extra_arrays is None:
         return sequences[:insertion_point]
     else:
-        return sequences[:insertion_point], positions[:insertion_point]
+        return sequences[:insertion_point], [
+            arr[:insertion_point] for arr in extra_arrays
+        ]
