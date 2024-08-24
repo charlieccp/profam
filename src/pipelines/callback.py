87d7b0cd245a60ad9512092558352dceda6ea277
alex-hh <alex.hhooker@gmail.com>
2024-08-24 17:48:31 +0100

    update pipeline callback

--- src/pipelines/callback.py
+++ src/pipelines/callback.py
@@ -4,28 +4,49 @@ from typing import Dict, Optional
 
 from lightning.pytorch.callbacks import Callback
 
+from src.data.preprocessing import BasePreprocessorConfig
+from src.data.utils import PromptBuilder
+from src.evaluators.base import SamplingEvaluator
+from src.models.samplers import ProFamSampler
+from src.pipelines.pipeline import GenerationsEvaluatorPipeline
+
 
 class SamplingEvaluationPipelineCallback(Callback):
-    def __init__(self, pipeline, evaluator, sampling_kwargs: Optional[Dict] = None):
+    def __init__(
+        self,
+        pipeline: GenerationsEvaluatorPipeline,
+        evaluator: SamplingEvaluator,
+        preprocessor: BasePreprocessorConfig,
+        max_tokens: int = 8192,
+        sampling_kwargs: Optional[Dict] = None,
+    ):
         self.pipeline = pipeline
         assert (
             not self.pipeline.save_to_file
         ), "Pipeline should not save to file during callback"
         self.evaluator = evaluator
         self.sampling_kwargs = sampling_kwargs or {}
+        self.preprocessor = preprocessor
+        self.max_tokens = max_tokens
 
     def on_validation_epoch_end(self, trainer, model):
         # run on val epoch end rather than train to stay in sync with other validation metrics
+        sampler = ProFamSampler(
+            "profam_sampler",
+            model,
+            prompt_builder=PromptBuilder(
+                self.preprocessor, model.tokenizer, max_tokens=self.max_tokens
+            ),
+            sampling_kwargs=self.sampling_kwargs,
+        )
         if trainer.is_global_zero:
             # https://lightning.ai/docs/pytorch/stable/visualize/logging_advanced.html#rank-zero-only
             # Q: how does logging work across ranks? if i log only from rank 0, what happens?
             all_metrics = defaultdict(list)
             t0 = time.time()
             results_df = self.pipeline.run(
-                model,
-                "profam_model",
+                sampler,
                 self.evaluator,
-                sampling_kwargs=self.sampling_kwargs,
                 verbose=False,
             )
             mean_results = results_df.mean().to_dict()
