ba6700cd46dc03d8222b06a283c9cc07f2f9a8a9
alex-hh <alex.hhooker@gmail.com>
2024-09-14 14:00:22 +0100

    fix coords prompt

--- src/models/inference.py
+++ src/models/inference.py
@@ -139,7 +139,7 @@ class ProFamSampler:
                 prompt["input_ids"].unsqueeze(0).to(self.model.device),
                 num_samples=num_samples,
                 input_seq_pos=prompt["seq_pos"].unsqueeze(0).to(self.model.device),
-                input_coords=prompt["coords"]
+                input_coords=prompt["coords"].unsqueeze(0).to(self.model.device).float()
                 if self.model.embed_coords
                 else None,  # n.b. preprocessing will produce coords for every input even when missing - careful about this
                 **self.sampling_kwargs
