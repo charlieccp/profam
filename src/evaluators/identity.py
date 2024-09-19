93adbf176628197f830098333864b450ede48062
alex-hh <alex.hhooker@gmail.com>
2024-09-19 13:03:30 +0100

    sequence recovery evaluator

---
+++ src/evaluators/identity.py
@@ -0,0 +1,49 @@
+"""We can implement both sequence recovery, requiring fixed length, and pairwise sequence identity."""
+from typing import Optional
+from src.evaluators.base import SamplingEvaluator
+from src.data.sequence import sequence_identity
+
+
+class SequenceIdentityEvaluator(SamplingEvaluator):
+    pass
+
+
+class SequenceRecoveryEvaluator(SamplingEvaluator):
+    def __init__(
+        self,
+        name: str,
+        seed: int = 52,
+        verbose: bool = False,
+        num_samples: Optional[int] = None,
+    ):
+        super().__init__(name, seed, num_samples)
+        self.verbose = verbose
+
+    def _evaluate_samples(
+        self,
+        prompt,
+        protein_document,
+        samples,
+        output_dir: Optional[str] = None,
+        device: Optional[str] = None,
+    ):
+        # TODO: compute recovery in unmasked prompt regions.
+        target_sequence = protein_document.representative.sequence
+        mismatched_lengths = 0
+        recoveries = []
+        if self.verbose:
+            print("Target")
+            print(target_sequence)
+        for i, seq in enumerate(samples):
+            if self.verbose and i == 0:
+                print(seq)
+            if len(seq) == len(target_sequence):
+                seq_id = sequence_identity(target_sequence, seq)
+                print("Seq ID", seq_id)
+                recoveries.append(seq_id)
+            else:
+                mismatched_lengths += 1
+        return {
+            "mean_recovery": sum(recoveries) / len(recoveries),
+            "mismatched_lengths": mismatched_lengths / len(samples),
+        }
